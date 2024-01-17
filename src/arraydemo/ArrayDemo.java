3ccf6d14a53a2d1035b384b46c5f6865a5be4aa8
Jiya Patel <pateljiy@sheridancollege.ca>
2024-01-17 09:45:05 -0500

    First commit

---
+++ src/arraydemo/ArrayDemo.java
@@ -0,0 +1,30 @@
+
+package arraydemo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jiyap
+ */
+public class ArrayDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Enter a word");
+        Scanner input = new Scanner(System.in);
+        
+        String myWord = input.nextLine();
+        char[] myLetters = new char[myWord.length()];
+        
+        for(int i=0; i<myLetters.length; i++){
+            
+            myLetters[i] = myWord.charAt(i);
+            
+        }
+        System.out.println("Printing in reverse");
+    }
+    
+}
